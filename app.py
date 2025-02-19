e13c9ad3d7706e393c53fca854eb97d444b27377
Jayvaghela0 <120317638+Jayvaghela0@users.noreply.github.com>
2025-02-19 22:43:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,8 +46,7 @@ def convert_to_mp4(m3u8_url, output_file):
 
 # Download endpoint
 @app.route('/download', methods=['GET'])
-def download_video():
-    return "welcome"
+def download_video()
     video_url = request.args.get('url')
     if not video_url:
         return jsonify({"error": "URL parameter is required"}), 400
