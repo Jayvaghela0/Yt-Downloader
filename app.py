8f35bf138bf5520c8abb187381aaddf9c7bcdb1b
Jayvaghela0 <120317638+Jayvaghela0@users.noreply.github.com>
2025-02-20 11:13:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,6 @@ def get_mp4():
     return jsonify({"mp4_url": mp4_url})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
